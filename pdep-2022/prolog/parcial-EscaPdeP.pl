06840055941e3a345f19c0839fbb78775b3caa38
Juan Nardi <102627173+juannardi13@users.noreply.github.com>
2022-08-07 23:51:28 -0300

    Arreglo parcial

--- pdep-2022/prolog/parcial-EscaPdeP.pl
+++ pdep-2022/prolog/parcial-EscaPdeP.pl
@@ -1,7 +1,3 @@
-% Simulacro Parcial - EscaPdeP
-
-% Base de conocimientos
-
 %persona(Apodo, Edad, Peculiaridades).
 persona(ale, 15, [claustrofobia, cuentasRapidas, amorPorLosPerros]).
 persona(agus, 25, [lecturaVeloz, ojoObservador, minuciosidad]).
@@ -26,85 +22,92 @@ sala(linternas, familiar(comics, 5)).
 sala(guerrasEstelares, familiar(futurista, 7)).
 sala(fundacionDelMulo, enigmatica([combinacionAlfanumerica, deLlave, deBoton])).
 
-% Punto 1
+% nivelDeDificultadDeLaSala/2: para cada sala nos dice su dificultad. Para las salas de experiencia terrorífica el nivel de dificultad es la cantidad de sustos menos 
+% la edad mínima para ingresar. Para las de experiencia familiar es 15 si es de una temática futurista y para cualquier otra temática es la cantidad de habitaciones.
+% El de las enigmáticas es la cantidad de candados que tenga.
 
 nivelDeDificultadDeLaSala(Sala, Dificultad) :-
-    sala(Sala, terrorifica(CantidadDeSustos, EdadMinima)),
-    Dificultad is CantidadDeSustos - EdadMinima.
+    sala(Sala, Tipo),
+    dificultadSegunTipo(Tipo, Dificultad).
+        
+dificultadSegunTipo(terrorifica(CantidadSustos, EdadMinima), Dificultad) :-
+    Dificultad is CantidadSustos - EdadMinima.
 
-nivelDeDificultadDeLaSala(Sala, 15) :-
-    sala(Sala, familiar(futuristica, _)).
+dificultadSegunTipo(familiar(futurista, _), 15).
+dificultadSegunTipo(familiar(_, CantidadHabitaciones), CantidadHabitaciones).
 
-nivelDeDificultadDeLaSala(Sala, Dificultad) :-
-    sala(Sala, familiar(Tematica, Dificultad)),
-    Tematica \= futuristica.
+dificultadSegunTipo(enigmatica(Candados), Dificultad) :-
+    length(Candados, Dificultad).
 
-nivelDeDificultadDeLaSala(Sala, Dificultad) :-
-    sala(Sala, enigmatica(Candados)),
-    length(Dificultad, Candados),
-
-%Punto 2
+% puedeSalir/2: una persona puede salir de una sala si la dificultad de la sala es 1 o si tiene más de 13 años y la dificultad es menor a 5. 
+% En ambos casos la persona no debe ser claustrofóbica.
 
 puedeSalir(Persona, Sala) :-
+    persona(Persona, _, _),
     not(esClaustrofobica(Persona)),
     nivelDeDificultadDeLaSala(Sala, 1).
 
 puedeSalir(Persona, Sala) :-
+    esMayorDe13(Persona),
     not(esClaustrofobica(Persona)),
-    edad(Persona, Edad),
     nivelDeDificultadDeLaSala(Sala, Dificultad),
-    Edad > 13,
     Dificultad < 5.
 
-esClaustrofobica(persona(_, _, Peculiaridades)) :-
+esClaustrofobica(Persona) :-
+    persona(Persona, _, Peculiaridades),
     member(claustrofobia, Peculiaridades).
 
-edad(persona(_, Edad, _), Edad).
+esMayorDe13(Persona) :-
+    persona(Persona, Edad, _),
+    Edad > 13.
 
-% Punto 3
+% tieneSuerte/2: una persona tiene suerte en una sala si puede salir de ella aún sin tener ninguna peculiaridad.
 
 tieneSuerte(Persona, Sala) :-
     puedeSalir(Persona, Sala),
     noTienePeculiaridades(Persona).
 
-noTienePeculiaridades(persona(_, _, [])).
+noTienePeculiaridades(Persona) :-
+    persona(Persona, _, Peculiaridades),
+    length(Peculiaridades, 0).
 
-% Punto 4
+% esMacabra/1: una empresa es macabra si todas sus salas son de experiencia terrorífica.
 
 esMacabra(Empresa) :-
-    forall(esSalaDe(NombreSala, Empresa), esTerrorifica(NombreSala)).
+    esSalaDe(_, Empresa),
+    forall(esSalaDe(Sala, Empresa), esSalaTerrorifica(Sala)).
 
-esTerrorifica(sala(_, terrorifica(_, _))).
+esSalaTerrorifica(Sala) :-
+    sala(Sala, terrorifica(_)).
 
-% Punto 5
+% empresaCopada/1: una empresa es copada si no es macabra y el promedio de dificultad de sus salas es menor a 4.
 
-esCopada(Empresa) :-
+empresaCopada(Empresa) :-
+    esSalaDe(_, Empresa),
     not(esMacabra(Empresa)),
-    promedioDeDificultad(Empresa, Dificultad),
-    Dificultad < 4.
+    promedioDificultadDeLasSalas(Empresa, PromedioDificultad),
+    PromedioDificultad < 4.
 
-promedioDeDificultad(Empresa, Dificultad) :-
-    cantidadDeSalas(Empresa, CantidadSalas),
-    sumatoriaDificultades(Empresa, Dificultades),
-    Dificultad is Dificultades / CantidadSalas.
+promedioDificultadDeLasSalas(Empresa, PromedioDificultad) :-
+    esSalaDe(_, Empresa),
+    findall(NivelDificultad, (esSalaDe(Sala, Empresa), nivelDeDificultadDeLaSala(Sala, NivelDificultad)), NivelesDificultad),
+    promedio(NivelesDificultad, PromedioDificultad).
 
-cantidadDeSalas(Empresa, CantidadSalas) :-
-    findall(Salas, esSalaDe(Sala, Empresa), CantidadSalas).
+promedio(ListaNumeros, Promedio) :-
+    sum_list(ListaNumeros, SumatoriaNumeros),
+    length(ListaNumeros, CantidadNumeros),
+    Promedio is SumatoriaNumeros // CantidadNumeros.
 
+% ¡Cada vez hay más salas y empresas! Conozcámoslas para agregarlas a nuestro sistema:
 
-sumatoriaDificultades(Empresa, Dificultades) :-
-    esSalaDe(Empresa, Salas),
-    findall(Dificultad, nivelDeDificultadDeLaSala(Salas, Dificultad), DificultadesSalas),
-    sum_list(DificultadesSalas, Dificultades).
+% La empresa supercelula es dueña de salas de escape familiares ambientadas en videojuegos.
+% La sala estrellasDePelea cuenta con 7 habitaciones pero lamentablemente no sabemos la cantidad que tiene su nueva sala choqueDeLaRealeza.
+% La empresa SKPista (fanática de un famoso escritor) es la dueña de una única sala terrorífica para mayores de 21 llamada miseriaDeLaNoche que nos asegura 150 sustos.
+% La nueva empresa que se suma a esta gran familia es Vertigo, pero aún no cuenta con salas.
 
-% Punto 6
-
-% empresa(NombreEmpresa, Salas).
-
-empresa(supercelula, [estrellasDePelea, choqueDeLaRealeza]).
-empresa(skPista, [miseriaDeLaNoche]).
-empresa(vertigo, []).
-
+esSalaDe(estrellasDePelea, supercelula).
+esSalaDe(choqueDeLaRealeza, supercelula).
 sala(estrellasDePelea, familiar(videojuegos, 7)).
-sala(choqueDeLaRealeza, familiar(videojuegos, _)).
+
+esSalaDe(miseriaDeLaNoche, skPista).
 sala(miseriaDeLaNoche, terrorifica(150, 21)).
